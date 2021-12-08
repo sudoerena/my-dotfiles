b065467692abeea747317de2161e7d77da960b4e
sudoerena <sudoerena@users.noreply.github.com>
2021-12-08 18:05:49 -0500

    fix install script to have rudimentary backups

--- src/install.sh
+++ src/install.sh
@@ -1,19 +1,29 @@
 WD=${PWD}
 
+mkdir ~/.dotfile-backups/
+BACKUP=~/.dotfile-backups/
+
 # bash
+cp -rp ~/.bashrc $BACKUP
 ln -s "${WD}"/config/bash.conf ~/.bashrc
 
 # i3wm
+cp -rp ~/.config/i3/config $BACKUP
 ln -s "${WD}"/config/i3.conf ~/.config/i3/config
 
 # nvim
+cp -rp ~/.config/nvim/init.vim $BACKUP
+cp -rp ~/.config/nvim/conf $BACKUP
 ln -s "${WD}"/config/nvim.conf ~/.config/nvim/init.vim
-ln -s "${WD}"/config/conf/ ~/.config/nvim/conf
+ln -s "${WD}"/config/conf/ ~/.config/nvim/
 
 # tmux
+cp -rp ~/.tmux.conf $BACKUP
 ln -s "${WD}"/config/tmux.conf ~/.tmux.conf
 
 # vim
+cp -rp ~/.vimrc $BACKUP
+cp -rp ~/.vim/conf $BACKUP
 ln -s "${WD}"/config/vim.conf ~/.vimrc
-ln -s "${WD}"/config/conf/ ~/.vim/conf
+ln -s "${WD}"/config/conf/ ~/.vim/
 
